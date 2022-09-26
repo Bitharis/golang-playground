09f1f8f7722ac83e1942110f09fa1718a6aaed68
unknown <bitharis@gmail.com>
2022-09-26 21:28:19 +0200

    add template fies home and about

--- main.go
+++ main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+
+	err := parsedTemplate.Execute(w, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World")
-
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		fmt.Printf("Byttes received: %d\n", n)
-	})
-
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.ListenAndServe(portNumber, nil)
 }
